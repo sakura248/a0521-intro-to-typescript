563a5f87069f7c7938cf47b707af4c4d02523f30
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-25 15:55:36 -0800

    modified

--- exercise/ts-basic.ts
+++ exercise/ts-basic.ts
@@ -14,7 +14,7 @@ const exerciseOne = () => {
     // • Hover over variables to inspect their types.
     // • Fix the error on line 18 by changing the value of pi to the expected type.
 
-    let pi = '3.14159'; // 右辺かえていいのけ？
+    let pi = '3.14159';
     let tau = +pi * 2;
     
     console.log('[Exercise 1.1]', `${tau} is ${pi} times two.`);
